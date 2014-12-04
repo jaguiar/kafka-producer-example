4f38e1a3bcb8835e49dd7b20b7c815edc5c5df2b
Jennifer Aguiar <jaguiar@users.noreply.github.com>
2014-12-04 13:53:08 +0100

    Clean comments

--- src/main/java/org/yaourtcorp/kafka/producer/example/EventsFactory.java
+++ src/main/java/org/yaourtcorp/kafka/producer/example/EventsFactory.java
@@ -1,6 +1,8 @@
 package org.yaourtcorp.kafka.producer.example;
 
-
+/*
+ * Interface for "event objects" factories: shall find a way to create a new event object to send to kafka. 
+ */
 public interface EventsFactory<T extends EventObject> {
 
 	/**
