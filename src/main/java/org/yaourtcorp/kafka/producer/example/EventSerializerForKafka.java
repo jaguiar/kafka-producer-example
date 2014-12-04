70697b29c03072da5bb82f2e0b8deb117b4e3d4a
Jennifer Aguiar <jaguiar@users.noreply.github.com>
2014-12-04 13:51:08 +0100

    Update EventSerializerForKafka.java

--- src/main/java/org/yaourtcorp/kafka/producer/example/EventSerializerForKafka.java
+++ src/main/java/org/yaourtcorp/kafka/producer/example/EventSerializerForKafka.java
@@ -2,6 +2,10 @@ package org.yaourtcorp.kafka.producer.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+/*
+ * Interface for serializer for "event objects" (will be called just before sending event to kafka)
+ * @see EventObject
+ */
 public interface EventSerializerForKafka<T extends EventObject> {
 
 	String serialize(T eventObject) throws JsonProcessingException;
